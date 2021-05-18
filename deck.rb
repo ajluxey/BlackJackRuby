bdfdb9b02878f7673e497a0ce1fde9ea8b3f9e6f
Алексей Ю, ККСО-17-3 <v_koralina@list.ru>
2021-05-18 23:10:53 +0500

    rework deck, cause now we have Card class

--- deck.rb
+++ deck.rb
@@ -4,12 +4,10 @@ class Deck
   attr_reader :cards
 
   def initialize
-    suits = ['^', '+', '<>', '<3']
-    values = Array(2..10).map(&:to_s) + %w[J Q K A]
-    @cards = suits.product(values).map { |suit, value| value + suit }
+    @cards = Array(0..3).product(Array(2..14)).map { |suit, value| Card.new(value, suit) }.shuffle
   end
 
   def give_card
-    @cards.delete_at(rand(@cards.length))
+    @cards.pop
   end
 end
