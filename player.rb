f24d4a718f532eeeebb610927e0a17fc3644a5a9
Алексей Ю, ККСО-17-3 <v_koralina@list.ru>
2021-05-18 23:39:23 +0500

    Interaction with cards is carried out through class Hand

--- player.rb
+++ player.rb
@@ -1,36 +1,19 @@
 # frozen_string_literal: true
 
 require_relative 'player_moves'
+require_relative 'hand.rb'
 
 class Player
-  attr_reader :name, :cards, :money, :cards_open
+  attr_reader :name, :money, :points, :cards_open
 
   include PlayerMoves
 
   def initialize(name)
     @name = name
     @money = 100
-    @cards = []
+    @hand = Hand.new()
+    @points = 0
     @cards_open = true
-  end
-
-  def calculate_points
-    points = 0
-    ace_flag = false
-
-    @cards.each do |card|
-      if card.to_i != 0
-        points += card.to_i
-      elsif card[0] == 'A'
-        ace_flag = true
-      else
-        points += 10
-      end
-    end
-    if ace_flag
-      points += points <= 10 ? 11 : 1
-    end
-    points
   end
 
   def bet(count)
@@ -48,8 +31,8 @@ class Player
   def skip; end
 
   def take_card(card)
-    @cards << card
-    raise "from #{self}: Player have more than 3 card" if @cards.length > 3
+    @hand.take_card(card)
+    @points = @hand.calculate_points
   end
 
   def open_cards
